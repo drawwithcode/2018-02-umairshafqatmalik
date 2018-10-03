f2fc16ac798675c3c37e57ee29bd826b215b05c0
umairshafqatmalik <43407634+umairshafqatmalik@users.noreply.github.com>
2018-10-03 12:22:06 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,36 @@
-function preload(){
-  // put preload code here
-}
+
+
+var i = 1;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  angleMode(DEGREES);
+
+  frameRate(30);
 }
 
 function draw() {
-  // put drawing code here
+
+  translate(width/2,height/2);
+  rotate(frameCount*2);
+
+	strokeWeight(0.2);
+  noFill();
+
+  stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/720));
+  line(width/10,0,(cos (frameCount*3)*width/5),(sin (frameCount*3)*width/5));
+
+strokeWeight(i);
+	stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/360));
+	line(width,0,width/7,width/7);
+
+i = i + 0.005
+
+
+  // Stops the draw function if a certain condition avaluates true
+  if (frameCount == 360) {
+    noLoop();
+  }
+
 }
